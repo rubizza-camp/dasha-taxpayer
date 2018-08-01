ed31e981db15d74eafc857eadff870b0a56a68f6
ilya-bitbucket <ilyakulikovitch@yandex.ru>
2018-08-01 12:38:03 +0300

    Authentication through social networks twitter and github

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'users/:id', to: 'users#show', as: 'user'
   get 'users', to: 'users#index', as: 'users'
