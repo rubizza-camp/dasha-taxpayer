3761326bc1c1e09881440ab95c146bac72ce6d34
Pharethi <aleksei_antihovich@mail.ru>
2018-08-02 08:45:23 +0300

    added factory_bot support

--- features/support/env.rb
+++ features/support/env.rb
@@ -26,6 +26,8 @@ require 'cucumber/rails'
 # 2) Set the value below to true. Beware that doing this globally is not
 # recommended as it will mask a lot of errors for you!
 #
+World(FactoryBot::Syntax::Methods)
+
 ActionController::Base.allow_rescue = false
 
 # Remove/comment out the lines below if your app doesn't have a database.
