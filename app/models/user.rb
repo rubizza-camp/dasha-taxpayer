b7f90c6bafc43d546b8c36bf746560ee93612a06
ilya-bitbucket <ilyakulikovitch@yandex.ru>
2018-08-03 10:24:10 +0300

    fixed cucumber error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :omniauthable
-
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
