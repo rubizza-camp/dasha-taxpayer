561f3a0af59bc68d3ce95d8cd637892359b8834f
ilya-bitbucket <ilyakulikovitch@yandex.ru>
2018-08-08 14:46:40 +0300

    Fixed migration error for refernces many-to-many models ActivityType and OrganizationForm

--- db/migrate/20180807071029_create_activity_types.rb
+++ db/migrate/20180807071029_create_activity_types.rb
@@ -2,7 +2,6 @@ class CreateActivityTypes < ActiveRecord::Migration[5.2]
   def change
     create_table :activity_types do |t|
       t.string :name
-      t.references :organization_form, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20180807111953_create_join_table_activity_types_organization_forms.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableActivityTypesOrganizationForms < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :activity_types, :organization_forms do |t|
-       t.index [:activity_type_id, :organization_form_id]
-       t.index [:organization_form_id, :activity_type_id]
-    end
-  end
-end
