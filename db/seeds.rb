7bb0148f5d683f70fcee5929019e7ce8d1f8f783
VDmitryO <vshk97@mail.ru>
2018-08-08 11:52:07 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(email: 'test@mail.ru', password: '123456', password_confirmation: '123456')
+
 OrganizationForm.create(name: 'Физическое лицо (самозанятость)')
 OrganizationForm.create(name: 'Индивидуальный Предприниматель')
 OrganizationForm.create(name: 'Частное предприятие')
 OrganizationForm.create(name: 'OOO')
 
-TaxationForm.create(name: 'Единый налог', description: '')
-TaxationForm.create(name: 'УСН')
+TaxationForm.create(name: 'Единый налог', organization_form: OrganizationForm.first)
+TaxationForm.create(name: 'УСН', organization_form: OrganizationForm.second)
+
+Organization.create(name: 'Название Организации',
+                    user: User.first,
+                    organization_form: OrganizationForm.first,
+                    taxation_form: TaxationForm.first)
