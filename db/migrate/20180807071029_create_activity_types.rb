b5add3f9897d1875f58fab55c2c14b16dba98128
ilya-bitbucket <ilyakulikovitch@yandex.ru>
2018-08-08 15:05:54 +0300

    add uniq: true for name in migration activity_type

--- db/migrate/20180807071029_create_activity_types.rb
+++ db/migrate/20180807071029_create_activity_types.rb
@@ -1,7 +1,7 @@
 class CreateActivityTypes < ActiveRecord::Migration[5.2]
   def change
     create_table :activity_types do |t|
-      t.string :name
+      t.string :name, uniq:true
 
       t.timestamps
     end
