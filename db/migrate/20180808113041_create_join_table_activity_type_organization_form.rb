8d2dba407a3845a46033127b27360bb18e23da86
ilya-bitbucket <ilyakulikovitch@yandex.ru>
2018-08-08 15:43:45 +0300

    Delete index in activity_types_organization_forms

--- db/migrate/20180808113041_create_join_table_activity_type_organization_form.rb
+++ db/migrate/20180808113041_create_join_table_activity_type_organization_form.rb
@@ -1,7 +1,5 @@
 class CreateJoinTableActivityTypeOrganizationForm < ActiveRecord::Migration[5.2]
   def change
-    create_join_table :activity_types, :organization_forms do |t|
-      t.index [:activity_type_id, :organization_form_id]
-    end
+    create_join_table :activity_types, :organization_forms
   end
 end
