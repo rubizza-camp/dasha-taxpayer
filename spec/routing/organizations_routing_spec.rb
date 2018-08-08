c7f9371785e3f528b25e804531f6252498dc94bf
Pharethi <aleksei_antihovich@mail.ru>
2018-08-08 11:56:40 +0300

    finished rspec fixes

--- spec/routing/organizations_routing_spec.rb
+++ spec/routing/organizations_routing_spec.rb
@@ -1,39 +1,38 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :routing do
-  describe "routing" do
-
-    it "routes to #index" do
+  describe 'routing' do
+    it 'routes to #index' do
       expect(get: '/organizations').to route_to('organizations#index')
     end
 
-    it "routes to #new" do
+    it 'routes to #new' do
       expect(get: '/organizations/new').to route_to('organizations#new')
     end
 
-    it "routes to #show" do
+    it 'routes to #show' do
       expect(get: '/organizations/1').to route_to('organizations#show', id: '1')
     end
 
-    it "routes to #edit" do
+    it 'routes to #edit' do
       expect(get: '/organizations/1/edit').to route_to('organizations#edit', id: '1')
     end
 
-    it "routes to #create" do
+    it 'routes to #create' do
       expect(post: '/organizations').to route_to('organizations#create')
     end
 
-    it "routes to #update via PUT" do
-      expect(put: '/organizations/1').to route_to("organizations#update", id: '1')
+    it 'routes to #update via PUT' do
+      expect(put: '/organizations/1').to route_to('organizations#update', id: '1')
     end
 
-    it "routes to #update via PATCH" do
+    it 'routes to #update via PATCH' do
       expect(patch: '/organizations/1').to route_to('organizations#update', id: '1')
     end
 
-    it "routes to #destroy" do
+    it 'routes to #destroy' do
       expect(delete: '/organizations/1').to route_to('organizations#destroy', id: '1')
     end
   end
