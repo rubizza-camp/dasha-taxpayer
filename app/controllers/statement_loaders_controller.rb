f9eec8652a1169f956aada108428ef7f49054a99
ilya-bitbucket <ilyakulikovitch@yandex.ru>
2018-08-15 17:48:35 +0300

    moved method select_type_tax controller StatementLoadersController in Service

--- app/controllers/statement_loaders_controller.rb
+++ app/controllers/statement_loaders_controller.rb
@@ -11,23 +11,7 @@ class StatementLoadersController < ApplicationController
     raise 'Exception Upload CSV' if params[:my_file].blank?
     csv       = CsvParserStatementService.new.parse(params[:my_file])
     array_obj = BgpbCsvHandlingService.new(csv).fetch
-    @data_statement = select_type_tax(params[:id]).new(array_obj)
+    @data_statement = GetTaxForOrganizationService.fetch(params[:id]).new(array_obj)
     render :show, data: @data_statement
-  end
-
-  def redirect_taxes
-    redirect_to taxes_path
-  end
-
-  private
-
-  def select_type_tax(organization_id)
-    @organization = Organization.find(organization_id)
-    case @organization.taxation_form.name
-    when 'УСН'
-      CalculateExtractForUsnService
-    when 'Единый налог'
-      CalculateExtractForSingleService
-    end
   end
 end
---
+++ app/services/get_tax_for_organization_service.rb
@@ -0,0 +1,11 @@
+class GetTaxForOrganizationService
+  def self.fetch(organization_id)
+    @organization = Organization.find(organization_id)
+    case @organization.taxation_form.name
+    when 'УСН'
+      CalculateExtractForUsnService
+    when 'Единый налог'
+      CalculateExtractForSingleService
+    end
+  end
+end
