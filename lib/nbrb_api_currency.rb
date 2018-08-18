1d84a781f9ab2bb0722527b6034d845021c392a5
Almix047 <Amix047@gmail.com>
2018-08-18 03:58:39 +0000

    nbrb-api v3

--- lib/nbrb_api_currency.rb
+++ lib/nbrb_api_currency.rb
@@ -10,29 +10,33 @@ module NbrbApi
       uri = URI.parse(URL + "/#{curr}")
       params = {ParamMode: '2', onDate: prepared_date(date), Periodicity: '0'}
       uri.query = URI.encode_www_form(params)
-
       response = Net::HTTP.get_response(uri)
-      p response.read_header
       prepared_data(response.body)
-      # if response.code == 200
-      #   prepared_data(response.body)
-      # else
-      #   puts "ERROR: #{response.read_header} (#{response.code})"
-      # end
     end
 
     def self.prepared_date(date)
-      year = Date.parse(date.to_s).year
-      month = Date.parse(date.to_s).month
-      day = Date.parse(date.to_s).day
+      year = parsing_year(date)
+      month = parsing_month(date)
+      day = parsing_day(date)
       "#{year}-#{month}-#{day}"
+    end
+
+    def self.parsing_day(date)
+      Date.parse(date.to_s).day
+    end
+
+    def self.parsing_month(date)
+      Date.parse(date.to_s).month
+    end
+
+    def self.parsing_year(date)
+      Date.parse(date.to_s).year
     end
 
     def self.prepared_data(content)
       rate_hash = JSON.parse(content)
       official_rate = rate_hash['Cur_OfficialRate']
       scales_foreign_currency_unit = rate_hash['Cur_Scale']
-      p rate_hash['Date']
       (official_rate / scales_foreign_currency_unit).round(2)
     end
   end
