f3fa70bfcb3054f83579af1b63107a61a13d1865
ilya-bitbucket <ilyakulikovitch@yandex.ru>
2018-08-20 21:04:02 +0300

    add testt for avtivity type and calculation form, add validates

--- app/models/activity_type.rb
+++ app/models/activity_type.rb
@@ -4,4 +4,5 @@ class ActivityType < ApplicationRecord
   has_and_belongs_to_many :organization_forms
   has_many :calculation_forms, dependent: :destroy
   has_one :activity, dependent: :destroy
+  validates :name, presence: true
 end
