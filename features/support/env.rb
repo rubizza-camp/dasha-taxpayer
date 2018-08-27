1030a6a75f4cd40283625e2b16a41d1ec4b2f002
ilya-bitbucket <ilyakulikovitch@yandex.ru>
2018-08-27 11:23:30 +0300

    delete comment for features/support/env.rb with required not exist file

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,6 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 require 'simplecov'
-# require './spec/support/simplecov'
 require 'cucumber/rails'
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
