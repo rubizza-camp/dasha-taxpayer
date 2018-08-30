599c7eaea9d4e6dca338d4c89701304f5fd7b29e
Almix047 <Amix047@gmail.com>
2018-08-30 22:27:36 +0000

    locale definition

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :organizations
-  post 'organization/:id/extract/new', to: 'statement_loaders#extract_tax', as: 'extract_tax'
-  get 'organization/:id/extract/new', to: 'statement_loaders#new', as: 'extract_new'
+  scope '(:locale)', locale: /en|ru/ do
+    resources :organizations
+    post 'organization/:id/extract/new', to: 'statement_loaders#extract_tax', as: 'extract_tax'
+    get 'organization/:id/extract/new', to: 'statement_loaders#new', as: 'extract_new'
 
-  get 'extract_tax/:id', to: 'statement_loaders#redirect_taxes'
+    get 'extract_tax/:id', to: 'statement_loaders#redirect_taxes'
 
-  resources :taxes
-  resources :survey, only: %i[new create]
+    resources :taxes
+    resources :survey, only: %i[new create]
 
-  root 'pages#index'
+    root 'pages#index'
+  end
 
   devise_for :users, controllers: {omniauth_callbacks: 'callbacks'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
